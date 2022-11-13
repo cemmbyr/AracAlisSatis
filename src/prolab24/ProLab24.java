3a9c0e1d062a5a7e08a56018e63187f295f2f518
cemmbyr <39888128+cemmbyr@users.noreply.github.com>
2022-11-13 15:56:28 +0300

    Update ProLab24.java

--- src/prolab24/ProLab24.java
+++ src/prolab24/ProLab24.java
@@ -4,8 +4,6 @@ package prolab24;
 
 public class ProLab24 {
 
-    // MUHAMMED EMRE KARA 160202094 - CEM BAYIR 160202066
-
     public static void main(String[] args) {
       new ArayuzGiris().setVisible(true);
         
